c4827d135943c1d57d1df1139a8feeb14784d857
pierre.prodel@etu.unilim.fr <Pierre@Asus>
2019-05-21 11:26:48 +0200

    Ajout de la class sprite

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Sprite.java
@@ -0,0 +1,64 @@
+package fr.unilim.iut.spaceinvaders;
+
+import fr.unilim.iut.spaceinvaders.utils.Dimension;
+import fr.unilim.iut.spaceinvaders.utils.Position;
+
+public abstract class Sprite {
+
+	public Sprite(Dimension dimension, Position origine, int vitesse) {
+		super();
+		this.dimension = dimension;
+		this.origine = origine;
+		this.vitesse = vitesse;
+	}
+
+	public Position origine;
+	public Dimension dimension;
+	public int vitesse;
+
+	public Sprite() {
+		super();
+	}
+
+	public boolean occupeLaPosition(int x, int y) {
+	    return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
+	}
+
+	public boolean estOrdonneeCouverte(int y) {
+	   return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
+	}
+
+	public boolean estAbscisseCouverte(int x) {
+	   return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
+	}
+
+	public int ordonneeLaPlusBasse() {
+	    return this.origine.ordonnee() - this.dimension.hauteur() + 1;
+	}
+
+	public int ordonneeLaPlusHaute() {
+		   return this.origine.ordonnee();
+	   }
+
+	public int abscisseLaPlusADroite() {
+		   return this.origine.abscisse() + this.dimension.longueur() - 1;
+	   }
+
+	public int abscisseLaPlusAGauche() {
+		   return this.origine.abscisse();
+	   }
+
+	public void seDeplacerVersLaDroite() {
+		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
+	}
+
+	public void seDeplacerVersLaGauche() {
+		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
+	}
+
+	public void positionner(int x, int y) {
+		   this.origine.changerAbscisse(x);
+		   this.origine.changerOrdonnee(y);
+	   }
+
+}
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -4,71 +4,11 @@ import fr.unilim.iut.spaceinvaders.utils.*;
 
 // Peut-être serait-il d'ailleurs intéressant donner une définition de la vitesse dans votre 
 //glossaire pour en connaître ses limites et sa mesure dans le cadre de votre application 
+// Peut-être est-ce du coup le bon moment pour donner une définition de sprite dans votre glossaire
 //A FAIRE
-public class Vaisseau {
+public class Vaisseau extends Sprite {
 
-    public Position origine;
-    public Dimension dimension;
-    public int vitesse ;
-
-    public Vaisseau(int longueur, int hauteur) {
-	    this(longueur, hauteur, 0, 0);
-    }
-
-   public Vaisseau(int longueur, int hauteur, int x, int y) {
-	   this(new Dimension(longueur, hauteur), new Position(x, y));
-    }
-
-   public Vaisseau(Dimension dimension, Position positionOrigine) {
-		this(dimension, positionOrigine, 1);
-	}
-   
     public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-		this.dimension = dimension;
-		this.origine = positionOrigine;
-		this.vitesse = vitesse;
-	}
-
-    public boolean occupeLaPosition(int x, int y) {
-	    return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
+	    super(dimension, positionOrigine, vitesse);
     }
-
-    public boolean estOrdonneeCouverte(int y) {
-	   return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-    }
-
-    public boolean estAbscisseCouverte(int x) {
-	   return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-    }
-
-    public int ordonneeLaPlusBasse() {
-	    return this.origine.ordonnee() - this.dimension.hauteur() + 1;
-    }
-
-    public int ordonneeLaPlusHaute() {
-	   return this.origine.ordonnee();
-   }
-
-   public int abscisseLaPlusADroite() {
-	   return this.origine.abscisse() + this.dimension.longueur() - 1;
-   }
-
-   public int abscisseLaPlusAGauche() {
-	   return this.origine.abscisse();
-   }
-
-   public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	}
-   
-
-   public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	}
-   
-   public void positionner(int x, int y) {
-	   this.origine.changerAbscisse(x);
-	   this.origine.changerOrdonnee(y);
-   }
-
 }
